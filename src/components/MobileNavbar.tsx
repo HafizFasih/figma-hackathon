2724823ef0f3cd3eb90ca0ba91e9f53db22c77ac
Muhammad Fasih <programmer.fasih.18@gmail.com>
2024-12-08 21:21:37 +0500

    Update MobileNavbar.tsx

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -7,16 +7,26 @@ const MobileNavbar = () => {
   const [display, setDisplay] = useState<boolean>(false);
   return (
     <>
-      <span onClick={() => setDisplay((val) => !val)} className="h-8 w-8 sm:hidden flex items-center justify-center">
+      <span
+        onClick={() => setDisplay((val) => !val)}
+        className="h-8 w-8 sm:hidden flex items-center justify-center"
+      >
         <IoMenuSharp className="h-4/5 w-4/5" />
       </span>
       <span
-        className={`${display ? "translate-x-0" : "-translate-x-full"} duration-500 absolute z-50 top-0 left-0 bg-black/90 h-screen w-4/5 sm:hidden inline-block`}
+        className={`${
+          display ? "translate-x-0" : "-translate-x-full"
+        } duration-500 fixed z-50 top-0 left-0 bg-black/90 h-screen w-4/5 sm:hidden inline-block`}
       >
         <ul className="h-full flex flex-col justify-evenly items-center">
           {["home", "about", "products", "cart"].map((val, ind) => (
             <Link key={ind} href={`/${val === "home" ? "" : val}`}>
-              <li className="capitalize font-clash text-lightGray">{val}</li>
+              <li
+                onClick={() => setDisplay(false)}
+                className="capitalize font-clash text-lightGray"
+              >
+                {val}
+              </li>
             </Link>
           ))}
         </ul>
